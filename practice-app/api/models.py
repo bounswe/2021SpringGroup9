d211a34db6cee0b5fd804284a0fef2c98785c318
zcanfes <zcanfes@gmail.com>
2021-06-01 12:27:26 +0300

    Add model

--- practice-app/api/models.py
+++ practice-app/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    story = models.CharField(max_length=1000)
+    username = models.CharField(max_length=32)
+    tags = models.JSONField()
+
+    def __str__(self):
+        return self.title
