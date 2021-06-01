349fc1c52043a490db6e351c2090178cfa523641
Ahmet Melih Aydoğdu <ahaydogdu@hotmail.com>
2021-06-01 22:03:19 +0300

    Model initialized. APIs will be integrated on this model.

--- practice-app/api/admin.py
+++ practice-app/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+
+from .models import Story
 # Register your models here.
+
+admin.site.register(Story)
--- practice-app/api/models.py
+++ practice-app/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Story(models.Model):
+    title = models.CharField(max_length=200)
+    story = models.CharField(max_length=1000)
+    name = models.CharField(max_length=200)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    location = models.CharField(max_length=200)
+    tag = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    notifyAdmin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
