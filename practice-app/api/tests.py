237260389dddecb241ff1ff94df6cd0ade65af18
omarr09 <56361427+omarr09@users.noreply.github.com>
2021-06-02 14:17:57 +0300

    Fixed a small problem in test URLs of api/test.py

--- practice-app/api/tests.py
+++ practice-app/api/tests.py
@@ -4,9 +4,9 @@ from django.test import TestCase, Client
 class GettingPlacesTestCase(TestCase):
     def test_get_places(self):
         c = Client()
-        response1 = c.get('nearbyplaces/123123')
+        response1 = c.get('/api/nearbyplaces/123123')
         self.assertEqual(response1.status_code, 404)
 
-        response2 = c.get('nearbyplaces/1')
+        response2 = c.get('/api/nearbyplaces/1')
         self.assertNotIn(response2.status_code, (404, 500))
         self.assertNotEqual(response2.json(), [])
