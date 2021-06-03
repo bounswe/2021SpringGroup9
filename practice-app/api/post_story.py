f6db43b74fe5b59c8ce90d78292b7971cb759765
mecolak <mstfec@gmail.com>
2021-06-03 18:03:16 +0300

    Added GET support for flagged stories(notifyAdmin == 1) on endpoint api/flagged_stories.

--- practice-app/api/post_story.py
+++ practice-app/api/post_story.py
@@ -18,10 +18,8 @@ tisane_key = env('TISANE_API_KEY')
 @csrf_exempt 
 def post_story(request):
 
-    if request.method == 'GET':
-        return HttpResponse('Frontend not yet implemented, you may use smth like Postman.')
-    elif request.method != 'POST':
-        return HttpResponse('Please use this end point with GET or POST.')
+    if request.method != 'POST':
+        return HttpResponse('Please use this end point with POST.', status = 400)
 
     # Seting various request parameters.
     request_body = json.loads(request.body)
@@ -40,11 +38,9 @@ def post_story(request):
         data = json.loads(response.read())
         conn.close()
 
-        #if('abuse' in data):
-        #    print(data['abuse'])
 
         #Create and save a post object. 
-        #If there was some possible abuse in the text, set the notify_admin flag.
+        #If there was some possible abuse in the text, set the notifyAdmin flag.
         created_post = Story(title = request_body['title'], story = request_body['story'], name = request_body['name'], 
             longitude = request_body['long'], latitude = request_body['lat'], location = request_body['location'], tag =" ", 
             notifyAdmin = ('abuse' in data) )
@@ -55,4 +51,19 @@ def post_story(request):
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
         return HttpResponse(status = 400)
+
+# Gets flagged stories (notifyAdmin == True) from the database.
+# Only used with get.
+def flagged_stories(request):
+    if request.method != 'GET':
+        return HttpResponse('Please use this end point with GET.')
+    query = Story.objects.filter(notifyAdmin = True)
+    response = {'flagged_stories': []}
+    for q in query:
+        response['flagged_stories'].append({
+            'id': q.id,
+            'title': q.title,
+            'story': q.story,
+        })
+    return JsonResponse(data = response)
     
--- practice-app/api/test_post_story.py
+++ practice-app/api/test_post_story.py
@@ -19,13 +19,18 @@ class PostStoryTestCase(TestCase):
     def test_creates_post(self): 
         dummy_body = json.dumps({'title':'TEST', 'story': 'STILL TESTING.', 'name':'USER', 
             'long':0, 'lat': 0 ,'location' : 'somwhere'})
-        response = self.client.post('/api/post_story/', data = dummy_body, content_type="application/json")
+        response = self.client.post('/api/story/', data = dummy_body, content_type="application/json")
         self.assertEqual(response.status_code, 200)
     
     def test_creates_flagged_post(self):
         dummy_body = json.dumps({'title':'TEST', 'story': 'Just shut up!', 'name':'USER', 
             'long':0, 'lat': 0 ,'location' : 'somwhere'})
-        response = self.client.post('/api/post_story/', data = dummy_body, content_type="application/json")
+        response = self.client.post('/api/story/', data = dummy_body, content_type="application/json")
         self.assertEqual(response.status_code, 200)
         notify_flag = Story.objects.get(id = response.json()['id']).notifyAdmin
         self.assertEqual(notify_flag, True)
+    
+    def test_gets_flagged_post(self):
+        response = self.client.get('/api/flagged_stories/')
+        self.assertEqual(response.status_code, 200)
+
