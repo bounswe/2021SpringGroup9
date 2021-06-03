eb19d67815dad0a928cab6d29745bea8e509b69c
unknown <kkadirkkalkan@hotmail.com>
2021-06-03 13:45:08 +0300

    modified views.py and urls.py

--- practice-app/api/urls.py
+++ practice-app/api/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path
+
 from . import views
 
-
-
 urlpatterns = [
+    path('city/<int:story_id>', views.get_cityinfo, name="city"),
 ]
+
--- practice-app/api/views.py
+++ practice-app/api/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseServerError
+from django.http import HttpResponseNotFound
+from django.http import JsonResponse
+import json
+from .models import Story
+import requests
+import environ
 
-# Create your views here.
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(env_file='../practice/.env')
+CITY_API_KEY = env('CITY_API_KEY')
+
+
+def get_cityinfo(request, story_id):
+  
+   
+
+    try:
+        story = Story.objects.get(id=story_id)
+    except Story.DoesNotExist:
+        return HttpResponseNotFound(f"There is no Story object with story_id {story_id}!")
+
+    
+    
+    try:
+        url = "https://wft-geo-db.p.rapidapi.com/v1/geo/locations/"+story.latitude+"-"+story.longitude+"/nearbyCities"
+
+        querystring = {"radius":"100"}
+
+        headers = {
+            'x-rapidapi-key': CITY_API_KEY,
+            'x-rapidapi-host': "wft-geo-db.p.rapidapi.com"
+            }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        info = json.loads(response.text)
+    except:
+        return HttpResponseServerError("Could not establish connection")
+
+    try:
+        cityinfo = {
+            
+            "name": info["data"][0]["name"],
+            "country": info["data"][0]["country"],
+            
+        }
+        return JsonResponse(cityinfo)
+    except:
+        return HttpResponseServerError("Error")
