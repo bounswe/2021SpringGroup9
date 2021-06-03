6371ce009eb5c365ddede446d7257c0f43635390
Ahmet Şentürk <iahmetsenturk@yahoo.com>
2021-06-04 01:55:25 +0300

    Bug fixed: parameter name on urls.py
    post_id in the urls.py has been changed to story_id to be in-line with vies.py

--- practice-app/api/urls.py
+++ practice-app/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/covid/<int:post_id>', views.get_covid_numbers, name='covid_numbers'),
+    path('posts/covid/<int:story_id>', views.get_covid_numbers, name='covid_numbers'),
 ]
