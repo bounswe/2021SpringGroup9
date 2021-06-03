8d9f135cbb13bdbdf45acf396761186da980dfed
zcanfes <zcanfes@gmail.com>
2021-06-03 20:15:50 +0300

    Add names to urls

--- practice-app/api/urls.py
+++ practice-app/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('getquote/<int:pid>', views.GetQuote.as_view()),
-    path('postquote/<int:pk>', views.FavQuote.as_view()),
+    path('getquote/<int:pid>', views.GetQuote.as_view(), name="get_quote"),
+    path('postquote/<int:pk>', views.FavQuote.as_view(), name="fav_quote"),
 ]
