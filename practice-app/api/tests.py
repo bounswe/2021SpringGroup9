f2771228cc5eedd3a6b0bae98b0cdd171baa1d7e
Melih Özcan <melihozcan@Melihs-MacBook-Air.local>
2021-06-03 14:20:18 +0300

    Tests modified to also cover non-existent requests

--- practice-app/api/tests.py
+++ practice-app/api/tests.py
@@ -2,12 +2,12 @@ from django.http import response
 from django.test import TestCase, Client
 
 
-class GettingPlacesTestCase(TestCase):
+class JokesTestCase(TestCase):
     def test_get_places(self):
         c = Client()
     
         response = c.get('/api/joke/ani')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
 
         response2 = c.get('/api/joke/animal')
         self.assertEqual(response2.status_code,200)
