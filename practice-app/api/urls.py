8e2abb51990a475c0b3aedaf967a4237695607b0
zcanfes <zcanfes@gmail.com>
2021-06-04 23:09:06 +0300

    Update the directories and change file names of tests and views

--- practice-app/api/urls.py
+++ practice-app/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import views_quote
 
 urlpatterns = [
-    path('quote/<int:pk>', views.GetQuoteTag.as_view(), name="get_quote_tag"),
-    path('postquote/<int:pk>', views.FavQuote.as_view(), name="fav_quote"),
-    path('quote/location/<int:pk>', views.GetQuoteLoc.as_view(), name="get_quote_loc"),
+    path('quote/<int:pk>', views_quote.GetQuoteTag.as_view(), name="get_quote_tag"),
+    path('postquote/<int:pk>', views_quote.FavQuote.as_view(), name="fav_quote"),
+    path('quote/location/<int:pk>', views_quote.GetQuoteLoc.as_view(), name="get_quote_loc"),
 ]
