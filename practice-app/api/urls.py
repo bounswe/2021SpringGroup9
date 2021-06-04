113c2f4bf62d761e41527fd0eee913c6df48dd18
zcanfes <zcanfes@gmail.com>
2021-06-04 21:27:23 +0300

    Update the urls for getting quote

--- practice-app/api/urls.py
+++ practice-app/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('getquote/<int:pk>', views.GetQuoteTag.as_view(), name="get_quote_tag"),
+    path('quote/<int:pk>', views.GetQuoteTag.as_view(), name="get_quote_tag"),
     path('postquote/<int:pk>', views.FavQuote.as_view(), name="fav_quote"),
-    path('getquoteloc/<int:pk>', views.GetQuoteLoc.as_view(), name="get_quote_loc"),
+    path('quote/location/<int:pk>', views.GetQuoteLoc.as_view(), name="get_quote_loc"),
 ]
