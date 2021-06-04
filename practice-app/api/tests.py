0810e97f14101353fed73ff0c598362a1154bd15
omarr09 <56361427+omarr09@users.noreply.github.com>
2021-06-04 21:36:50 +0300

    Fixed problems in tests.py
    Added setUp method and put test cases in separate methods

--- practice-app/api/tests.py
+++ practice-app/api/tests.py
@@ -1,15 +1,52 @@
 from django.test import TestCase, Client
+from .models import Story
 
 
 class GettingPlacesTestCase(TestCase):
-    def test_get_places(self):
-        c = Client()
-        response1 = c.get('/api/nearbyplaces/123123')
-        self.assertEqual(response1.status_code, 404)
-        
-        response2 = c.get('/api/nearbyplaces/1')
-        self.assertNotIn(response2.status_code, (404, 500))
-        self.assertNotEqual(response2.json(), [])
+    def setUp(self):
+        Story.objects.create(
+            title="My Story",
+            story="Story body",
+            name="John",
+            latitude=37.184,
+            longitude=-123.798,
+            location="California",
+            tag="Travel",
+            notifyAdmin=False
+        )
 
-        response3 = c.get('/api/nearbyplaces/asdfgh')
-        self.assertEqual(response3.status_code, 404)
+        Story.objects.create(
+            title="Your Story",
+            story="Your Story body",
+            name="Johanna",
+            latitude=37.184,
+            longitude=-123.798,
+            location="California",
+            tag="Travel",
+            notifyAdmin=False
+        )
+
+    
+    def test_existing_place(self):
+        """ Tests getting nearby places of a story which exists in the database """
+        c = Client()
+        response = c.get('/api/nearbyplaces/1')
+        self.assertNotIn(response.status_code, (404, 500))
+        self.assertNotEqual(response.json(), [])
+
+    
+    def test_non_existing_place(self):
+        """ Tests getting nearby places of a story which does not in the database
+        This should return 404 """
+        c = Client()
+        response = c.get('/api/nearbyplaces/123123')
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_incorrect_api_request(self):
+        """ Tests calling the API method with an incorrect parameter.
+        This should return 404 """
+        c = Client()
+        response = c.get('/api/nearbyplaces/asdfgh')
+        self.assertEqual(response.status_code, 404)
+    
