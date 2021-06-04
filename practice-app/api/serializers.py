7fdc784d83ef3918960cc1e1cf8a03527e353a02
mecolak <mstfec@gmail.com>
2021-06-04 23:53:27 +0300

    Deleted serializers.py

--- practice-app/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Story
-from .models import Location
-
-class StorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Story
-        fields = ['id' ,'date', 'name', 'location', 'tag', 'title', 'story', 'notifyAdmin','latitude', 'longitude']
-        read_only_fields = ['notifyAdmin','latitude', 'longitude']
