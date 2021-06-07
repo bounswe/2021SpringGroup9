5c3f2c69b2edfee0ca1ee9dbf41cce038d9da186
Ahmet Melih Aydoğdu <ahaydogdu@hotmail.com>
2021-06-07 15:59:55 +0300

    env is changed.

--- practice-app/practice/settings.py
+++ practice-app/practice/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-env = environ.Env(DEBUG=(bool, False))
-environ.Env.read_env()
+env = environ.Env(DEBUG=(bool, True))
+environ.Env.read_env('.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
