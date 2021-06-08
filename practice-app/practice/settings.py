46eeb67f279cf292b2cc259c9bab89ca80623a41
Ahmet Melih Aydoğdu <ahaydogdu@hotmail.com>
2021-06-08 23:14:40 +0300

    DEBUG mode is changed to True. Otherwise, django cannot handle static files.

--- practice-app/practice/settings.py
+++ practice-app/practice/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-env = environ.Env(DEBUG=(bool, False))
+env = environ.Env(DEBUG=(bool, True))
 environ.Env.read_env('.env')
 SECRET_KEY = env('SECRET_KEY')
 
