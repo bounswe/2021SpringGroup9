ee6dfd57b6d69279b29bfbfbfb75f704b43723e3
mecolak <mstfec@gmail.com>
2021-11-03 19:24:50 +0300

    Frontend initial commit.

---
+++ postory/frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://user-images.githubusercontent.com/52619088/139734287-271be973-fb00-44c1-a1bc-d561cb6e8e42.jpeg" className="App-logo" alt="logo" />
+        <p>
+          POSTORY
+        </p>
+        <a
+        >
+          POSTORY app
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
