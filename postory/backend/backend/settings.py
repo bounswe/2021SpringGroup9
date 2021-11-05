5e4dd9feb1c1f3bfd67d9a8cbbfe3009a0ff8b33
Ahmet Melih Aydoğdu <ahaydogdu@hotmail.com>
2021-11-05 23:01:10 +0300

    model created

--- postory/backend/backend/settings.py
+++ postory/backend/backend/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'mongo-test',
+        'CLIENT': {
+           'host': f'mongodb://{env("MONGO_USERNAME")}:{env("MONGO_PASSWORD")}@0.0.0.0:2717/',
+        }
     }
 }
 
--- postory/backend/post_endpoint/models.py
+++ postory/backend/post_endpoint/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    story = models.CharField(max_length=1000)
+    owner = models.CharField(max_length=200)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    location = models.CharField(max_length=200)
+    tags = models.CharField(max_length=200) # models.ListField
+    postDate = models.DateTimeField(auto_now_add=True)
+    # multimedia 
+    # comments = models.ListField
+    editDate = models.DateTimeField(auto_now_add=True)
+    storyDate = models.DateTimeField()
+    viewCount = models.IntegerField(default=0)
+    # likeList = models.ListField
+
+
+class Comment(models.Model):
+    pass
+
+class Like(models.Model):
+    pass
