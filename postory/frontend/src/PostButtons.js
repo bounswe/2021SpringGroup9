85e248fc1c6d78c78a61117c50dc619a649caef9
Ahmet Şentürk <iahmetsenturk@yahoo.com>
2021-11-05 21:59:49 +0300

    Update on PostButtons, separator is added
    A separator is added between the buttons. Like button is also updated, now it shows a dummy like count next to the button (i.e., if button is pressed it will show 1 and 0 otherwise)

--- postory/frontend/src/PostButtons.js
+++ postory/frontend/src/PostButtons.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { render } from 'react-dom'
 
 const imagesPath = {
     disliked: "https://user-images.githubusercontent.com/35606355/140415129-e3f06692-9bf1-4796-878f-b59126102994.png",
@@ -6,16 +7,24 @@ const imagesPath = {
     comment: "https://user-images.githubusercontent.com/35606355/140415650-a57c1153-ae17-44db-92be-2a74f7f6f1c1.png",
     share: "https://user-images.githubusercontent.com/35606355/140418385-09486b42-d1f6-4d8c-9edc-408f80fea3d1.png",
     save: "https://user-images.githubusercontent.com/35606355/140419635-2f3f4992-c95a-470c-baba-c54252f0de5c.png",
-    report: "https://user-images.githubusercontent.com/35606355/140419056-b4f8af60-f0ed-48cb-83e2-0c18923cbe8a.png"
+    report: "https://user-images.githubusercontent.com/35606355/140419056-b4f8af60-f0ed-48cb-83e2-0c18923cbe8a.png",
+    verticalSeperator: "https://user-images.githubusercontent.com/35606355/140555305-60463bba-b681-4c1b-8c17-9b29e6126933.png",
   }
   
 class LikeButton extends React.Component {
-
-  state = {
-    liked: false
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            liked: false,
+            likeNumber: 0
+        };
+      }
+//   state = {
+//     liked: false,
+//     likeNumber: 0
+//   }
   toggleImage = () => {
-    this.setState(state => ({ liked: !state.liked }))
+    this.setState({ liked: !this.state.liked, likeNumber: this.state.liked ? 0 : 1 });
   }
 
   getImageName = () => this.state.liked ? 'liked' : 'disliked'
@@ -23,8 +32,11 @@ class LikeButton extends React.Component {
   render() {
     const imageName = this.getImageName();
     return (
-      <div>
+      <div class= "row">
         <img style={{maxWidth: '50px'}} src={imagesPath[imageName]} onClick={this.toggleImage} />
+        <div>
+          {this.state.likeNumber}
+        </div>
       </div>
     );
   }
@@ -74,14 +86,28 @@ class ReportButton extends React.Component {
     }
 }
 
+function VerticalSeperator() {
+    return(
+        <div>
+          <img style={{maxWidth: '50px'}} src={imagesPath["verticalSeperator"]} />
+        </div>
+    );
+    
+}
+
+
 class PostButtons extends React.Component {
     render(){
         return(<div >
             <div class= "row">
                 <LikeButton></LikeButton>
+                <VerticalSeperator></VerticalSeperator>
                 <CommentButton></CommentButton>
+                <VerticalSeperator></VerticalSeperator>
                 <ShareButton></ShareButton>
+                <VerticalSeperator></VerticalSeperator>
                 <SaveButton></SaveButton>
+                <VerticalSeperator></VerticalSeperator>
                 <ReportButton></ReportButton>
             </div>
         </div>);
