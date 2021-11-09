a593f8cac0c29dee7eb8667a505de441f9565b51
Ahmet Şentürk <iahmetsenturk@yahoo.com>
2021-11-09 11:14:19 +0300

    input and output mechanism implemented
    A text box that gets the input (tag) and an add button that adds this input to the state array have been implemented.
    State array has also been rendered to allow user to see the selected tags so far.

--- postory/frontend/src/TagChooser.js
+++ postory/frontend/src/TagChooser.js
@@ -8,17 +8,53 @@ class TagChooser extends React.Component{
         super(props);
         
         this.state = {
+            value: '',
             selectedTags: [] 
         };
 
     }
 
+    onChangeValue = event => {
+        this.setState({ value: event.target.value });
+      };
+
+    addTagToSelectedTags = () => {
+        this.setState(state => {
+          const selectedTags = state.selectedTags.concat(state.value);
+     
+          return {
+            selectedTags,
+            value: '',
+          };
+        });
+      };
+
     render(){
         return(
             <div>
-                
+                <div class= "row">
+                <input
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.onChangeValue}
+                />
+                <button
+                    type="button"
+                    onClick={this.addTagToSelectedTags}
+                    disabled={!this.state.value}
+                >
+                Add
+                </button>     
+                </div>
+                Selected Tags
+                <ul>
+                    {this.state.selectedTags.map(item => (
+                        <li key={item}>{item}</li>
+                    ))}
+                 </ul>  
             </div>
         );
     }
 }
 
+export default TagChooser;
