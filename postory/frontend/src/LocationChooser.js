2a765dc036f805a8965953e70cf7097b8782af4f
Ahmet Şentürk <iahmetsenturk@yahoo.com>
2021-11-09 19:26:44 +0300

    state variables and input mechanism implemented
    A text box that gets the input (location in which the story have occurred) and an add button that adds this input to the state array have been implemented.
    State array has also been rendered to allow user to see the entered locations so far.
    State variable named 'value' holds the last location entered by the user.

--- postory/frontend/src/LocationChooser.js
+++ postory/frontend/src/LocationChooser.js
@@ -1,12 +1,59 @@
 import React from 'react'
-
-import './TagChooser.css'
+import './LocationChooser.css'
 
 class LocationChooser extends React.Component{
     constructor(props){
         super(props);
 
+        this.state = {
+            value: '',
+            selectedLocations: [] 
+        };
+
     }
+
+    onChangeValue = event => {
+        this.setState({ value: event.target.value });
+      };
+
+    addTagToSelectedLocations = () => {
+        this.setState(state => {
+          const selectedLocations = state.selectedLocations.concat(state.value);
+
+          return {
+            selectedLocations,
+            value: '',
+          };
+        });
+      };
+
+    render(){
+        return(
+            <div>
+                <div class= "row">
+                <input
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.onChangeValue}
+                />
+                <button
+                    type="button"
+                    onClick={this.addTagToSelectedLocations}
+                    disabled={!this.state.value}
+                >
+                Add
+                </button>     
+                </div>
+                Selected Locations
+                <ul>
+                    {this.state.selectedLocations.map(item => (
+                        <li key={item}>{item}</li>
+                    ))}
+                 </ul>  
+            </div>
+        );
+    }
+
 }
 
 export default LocationChooser;
