46f78ecd27be4036b432ff487ad0fa4d68dbb4c3
Ahmet Şentürk <iahmetsenturk@yahoo.com>
2021-11-09 09:58:54 +0300

    Initial commit
    TagChooser.js and TagChooser.css are added. Also the baseline of the component class for the TagChooser has been created

---
+++ postory/frontend/src/TagChooser.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Icon from '@mdi/react'
+import { mdiPlus } from '@mdi/js';
+import './TagChooser.css'
+
+class TagChooser extends React.Component{
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            selectedTags: [] 
+        };
+
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
