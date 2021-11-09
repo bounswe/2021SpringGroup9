8c7001f8aafdd0d2416e8b9e8a9d33ca94803f73
Ahmet Şentürk <iahmetsenturk@yahoo.com>
2021-11-09 13:44:43 +0300

    input and output mechanism implemented
    A small text box that gets the input (people that will be tagged to the post) from the user and an add button that adds this input to the state array have been implemented.
    State array that holds all the people that added by the user has also been rendered to allow user to see the added people so far.

--- postory/frontend/src/PeopleChooser.js
+++ postory/frontend/src/PeopleChooser.js
@@ -12,4 +12,48 @@ class PeopleChooser extends React.Component{
 
     }
 
+    onChangeValue = event => {
+        this.setState({ value: event.target.value });
+      };
+
+    addTagToSelectedPeople = () => {
+        this.setState(state => {
+          const selectedPeople = state.selectedPeople.concat(state.value);
+
+          return {
+            selectedPeople,
+            value: '',
+          };
+        });
+      };
+
+    render(){
+        return(
+            <div>
+                <div class= "row">
+                <input
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.onChangeValue}
+                />
+                <button
+                    type="button"
+                    onClick={this.addTagToSelectedPeople}
+                    disabled={!this.state.value}
+                >
+                Add
+                </button>     
+                </div>
+                Selected People
+                <ul>
+                    {this.state.selectedPeople.map(item => (
+                        <li key={item}>{item}</li>
+                    ))}
+                 </ul>  
+            </div>
+        );
+    }
+
 }
+
+export default PeopleChooser;
