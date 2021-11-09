5e567bda504265ab24a81983a914986c6e25625c
Ahmet Şentürk <iahmetsenturk@yahoo.com>
2021-11-09 19:01:19 +0300

    Initial commit
    LocationChooser.js and LocationChooser.css are added. Also the baseline of the component class for the LocationChooser has been created with only its constructor

---
+++ postory/frontend/src/LocationChooser.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './TagChooser.css'
+
+class LocationChooser extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+}
+
+export default LocationChooser;
