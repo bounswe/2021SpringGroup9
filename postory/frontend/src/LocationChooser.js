ded8894fb4fddb9c6c8e32d1d719acd6f1cbd7b0
Ahmet Şentürk <iahmetsenturk@yahoo.com>
2021-11-09 19:46:33 +0300

    clear and remove buttons are added
    Following buttons have been implemented:
    1 - A clear button that allows users to the clear all of the selected/added locations previously
    2 - A remove button next to each entered location previously (listed under the 'Selected Locations'). It  allows users to remove the specific location that they added before as opposed to clear all button. It removes the selected location element from the state's array on click
    
    Also some comments have been added.

--- postory/frontend/src/LocationChooser.js
+++ postory/frontend/src/LocationChooser.js
@@ -6,17 +6,38 @@ class LocationChooser extends React.Component{
         super(props);
 
         this.state = {
-            value: '',
-            selectedLocations: [] 
+            value: '', // Holds the last entered location as an input
+            selectedLocations: [] // Holds all the locations that are entered by the user
         };
 
     }
 
+    clearAllSelectedLocations = () => {
+        {/* Called when user clicks on clear all button to clear all the locations that added to the post before*/}
+        this.setState({ selectedLocations: [] });
+    };
+
+    removeLocation= i => {
+        {/* Called when user clicks on x button next to each location added previously to the post.
+            It filters the location that wanted to be removed from selectedLocation array returns it */}
+        this.setState(state => {
+          const selectedLocations = state.selectedLocations.filter((item, j) => i !== j);
+
+          return {
+            selectedLocations,
+          };
+        });
+      };
+
     onChangeValue = event => {
+        {/* Called when when there is change in the input box allows user to enter location*/}
         this.setState({ value: event.target.value });
       };
 
+    
     addTagToSelectedLocations = () => {
+        {/* Called when user clicks on add button to add the last entered location (state.value) the post
+            It then contats the previous selectedLocations list with the last entered location and returns */}
         this.setState(state => {
           const selectedLocations = state.selectedLocations.concat(state.value);
 
@@ -42,12 +63,25 @@ class LocationChooser extends React.Component{
                     disabled={!this.state.value}
                 >
                 Add
-                </button>     
+                </button>
+                <button 
+                    type="button" 
+                    onClick={this.clearAllSelectedLocations}
+                >
+                Clear All 
+                </button>       
                 </div>
                 Selected Locations
                 <ul>
-                    {this.state.selectedLocations.map(item => (
-                        <li key={item}>{item}</li>
+                    {this.state.selectedLocations.map((item, index) => (
+                        <li key={item}>{item}
+                            <button
+                                type="button"
+                                onClick={() => this.removeLocation(index)}
+                            >
+                                x
+                            </button>
+                        </li>
                     ))}
                  </ul>  
             </div>
