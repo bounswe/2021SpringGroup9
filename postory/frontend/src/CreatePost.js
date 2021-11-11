045fa06fa127ff413fff6f2bebca3e914241f65a
mecolak <mstfec@gmail.com>
2021-11-11 14:56:06 +0300

    Added feedback messages of post information and when user sends a post.

--- postory/frontend/src/CreatePost.js
+++ postory/frontend/src/CreatePost.js
@@ -9,8 +9,9 @@ import TimeChooser from './TimeChooser';
 import TagChooser from './TagChooser';
 import PeopleChooser from './PeopleChooser'
 import LocationChooser from './LocationChooser'
+import {Snackbar} from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 
-import styles from './App.css';
 
 class CreatePost extends React.Component{
     constructor(props){
@@ -24,13 +25,16 @@ class CreatePost extends React.Component{
         this.handleChildObjectSend = this.handleChildObjectSend.bind(this);
         this.select = this.select.bind(this);
         this.sendToBackend = this.sendToBackend.bind(this);
+        this.handleSuccessClose = this.handleSuccessClose.bind(this);
+        this.handleInfoClose = this.handleInfoClose.bind(this);
     }
 
     handleChildObjectSend(whichComponent, childObj){
         this.setState(state => {
             let newObj =  JSON.parse(JSON.stringify(state));
             newObj.postData[whichComponent] = childObj;
-
+            newObj['addedInformation'] = true;
+            newObj['whichInfo'] = whichComponent;
             console.log(newObj);
 
             return newObj;
@@ -48,8 +52,29 @@ class CreatePost extends React.Component{
         });
     }
 
+    handleSuccessClose(){
+        this.setState(state => {
+            let newObj =  JSON.parse(JSON.stringify(state));
+            newObj['success'] = false;
+            return newObj;
+        });
+    }
+
+    handleInfoClose(){
+        this.setState(state => {
+            let newObj =  JSON.parse(JSON.stringify(state));
+            newObj['addedInformation'] = false;
+            return newObj;
+        });
+    }
+
     sendToBackend(){
         // send this.state.postData to backend
+        this.setState(state => {
+            let newObj =  JSON.parse(JSON.stringify(state));
+            newObj['success'] = true;
+            return newObj;
+        });
         console.log(JSON.stringify(this.state.postData) + 'failed');
     }
 
@@ -74,6 +99,19 @@ class CreatePost extends React.Component{
                     <button class = "createPostBtn" onClick = {() => {this.select('Tags')}}>Tags</button>
                 </div>
 
+
+                <Snackbar open={this.state.addedInformation} autoHideDuration={1000} onClose={this.handleInfoClose} >
+                    <Alert onClose={this.handleInfoClose} severity="success" sx={{ width: '100%' }}>
+                        Information about {this.state.whichInfo} is added to your post.
+                    </Alert>
+                </Snackbar>
+
+                <Snackbar open={this.state.success} autoHideDuration={1000} onClose={this.handleSuccessClose} >
+                    <Alert onClose={this.handleSuccessClose} severity="success" sx={{ width: '100%' }}>
+                        Your post is successfully created.
+                    </Alert>
+                </Snackbar>
+                
                 <Link to= "/createPost" variant = "v6">
                     <Icon onClick = { this.sendToBackend} class = "circle homePageCreatePostButton" path={mdiSend}
                         title="Location"
@@ -81,6 +119,8 @@ class CreatePost extends React.Component{
                         color="black"
                     />
                 </Link>
+                
+            
             </div>
             </header>
         );
