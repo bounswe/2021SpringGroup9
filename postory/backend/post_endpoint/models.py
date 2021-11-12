b9496307ffa4f3f0faddac96ec02817489291ab7
Ahmet Melih Aydoğdu <ahaydogdu@hotmail.com>
2021-11-12 22:49:10 +0300

    manytomanyfields can be blank

--- postory/backend/post_endpoint/models.py
+++ postory/backend/post_endpoint/models.py
@@ -18,11 +18,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     story = models.TextField()
     owner = models.CharField(max_length=200)
-    locations = models.ManyToManyField(Location)
-    tags = models.ManyToManyField(Tag)
-    images = models.ManyToManyField(Image)
+    locations = models.ManyToManyField(Location, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    images = models.ManyToManyField(Image, blank=True)
     postDate = models.DateTimeField(auto_now_add=True)
-    # multimedia 
     # comments = models.ListField
     editDate = models.DateTimeField(auto_now_add=True)
     storyDate = models.DateTimeField()
