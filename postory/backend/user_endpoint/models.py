564b1e2156a18c895e375b7cf81418d3faf65f6d
Ahmet Melih Aydoğdu <ahaydogdu@hotmail.com>
2021-11-25 19:52:54 +0300

    deployment fix

--- postory/backend/user_endpoint/models.py
+++ postory/backend/user_endpoint/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from postory.backend.post_endpoint.models import Post, Comment
+from post_endpoint.models import Post, Comment
 
 
 class User(models.Model):
@@ -11,9 +11,9 @@ class User(models.Model):
     email = models.EmailField(max_length=200)
     followedUsers = models.ManyToManyField("self")
     followerUsers = models.ManyToManyField("self")
-    posts = models.ManyToManyField(Post)
-    savedPosts = models.ManyToManyField(Post)
-    likedPosts = models.ManyToManyField(Post)
+    posts = models.ManyToManyField(Post, related_name="posts")
+    savedPosts = models.ManyToManyField(Post, related_name="savedPosts")
+    likedPosts = models.ManyToManyField(Post, related_name="likedPosts")
     comments = models.ManyToManyField(Comment)
     isBanned = models.BooleanField(default=False)
     isAdmin = models.BooleanField(default=False)   
