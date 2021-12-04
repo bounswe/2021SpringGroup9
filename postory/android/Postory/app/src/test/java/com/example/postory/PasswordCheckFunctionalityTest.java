2bb55e6b2f2a64ca9b8ddab394f02d1becac690e
Niyazi Ülke <niyaziulke@gmail.com>
2021-12-04 13:21:47 +0300

    Password check functionality for register is completed.

--- postory/android/Postory/app/src/test/java/com/example/postory/PasswordCheckFunctionalityTest.java
+++ postory/android/Postory/app/src/test/java/com/example/postory/PasswordCheckFunctionalityTest.java
@@ -11,13 +11,15 @@ import org.junit.Test;
  */
 public class PasswordCheckFunctionalityTest {
     String shortPass = "A1b2_2";
-    String noCapital = "prk32lxxx234_aa";
+    String noCapital = "prk32lxxx234_aa*";
     String noDigit = "a*B*c*qwerty_javakotlin";
+    String noSpecial = "aB12cDefKlmno2341";
     String valid = "tHis_PassWord_iSs_Super_2021";
 
     PasswordController shortController = new PasswordController(shortPass);
     PasswordController noCapitalController = new PasswordController(noCapital);
     PasswordController noDigitController = new PasswordController(noDigit);
+    PasswordController noSpecialController = new PasswordController(noSpecial);
     PasswordController validController = new PasswordController(valid);
 
     @Test
@@ -25,6 +27,7 @@ public class PasswordCheckFunctionalityTest {
         assertFalse(shortController.lengthEnough());
         assertTrue(noCapitalController.lengthEnough());
         assertTrue(noDigitController.lengthEnough());
+        assertTrue(noSpecialController.lengthEnough());
         assertTrue(validController.lengthEnough());
     }
 
@@ -33,6 +36,7 @@ public class PasswordCheckFunctionalityTest {
         assertTrue(shortController.containsCapital());
         assertFalse(noCapitalController.containsCapital());
         assertTrue(noDigitController.containsCapital());
+        assertTrue(noSpecialController.containsCapital());
         assertTrue(validController.containsCapital());
     }
 
@@ -41,14 +45,23 @@ public class PasswordCheckFunctionalityTest {
         assertTrue(shortController.containsNumber());
         assertTrue(noCapitalController.containsNumber());
         assertFalse(noDigitController.containsNumber());
+        assertTrue(noSpecialController.containsNumber());
         assertTrue(validController.containsNumber());
     }
-
+    @Test
+    public void checkSpecialControllerFunction(){
+        assertTrue(shortController.containsSpecial());
+        assertTrue(noCapitalController.containsSpecial());
+        assertTrue(noDigitController.containsSpecial());
+        assertFalse(noSpecialController.containsSpecial());
+        assertTrue(validController.containsSpecial());
+    }
     @Test
     public void checkAllControllerFunction(){
         assertFalse(shortController.allControls());
         assertFalse(noCapitalController.allControls());
         assertFalse(noDigitController.allControls());
+        assertFalse(noSpecialController.allControls());
         assertTrue(validController.allControls());
     }
 }
