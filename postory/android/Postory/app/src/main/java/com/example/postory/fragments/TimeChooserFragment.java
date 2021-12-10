f252e44b077135e48669043cf9d6abdc01d754ea
Niyazi Ülke <niyaziulke@gmail.com>
2021-12-10 20:59:05 +0300

    timeChooser layout design completed

--- postory/android/Postory/app/src/main/java/com/example/postory/fragments/TimeChooserFragment.java
+++ postory/android/Postory/app/src/main/java/com/example/postory/fragments/TimeChooserFragment.java
@@ -1,14 +1,23 @@
 package com.example.postory.fragments;
-
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import com.example.postory.R;
+import com.example.postory.utils.HourMinuteHandler;
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,10 +31,40 @@ public class TimeChooserFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
+
+    private Button startTimePicker;
+    private Button endTimePicker;
+
+    View monthOuterLayout;
+    View monthInnerLayout;
+    View dayOuterLayout;
+    View dayInnerLayout;
+    View timeOuterLayout;
+    View timeInnerLayout;
+    private SwitchCompat monthSwitch;
+    private SwitchCompat daySwitch;
+    private SwitchCompat timeSwitch;
+
+    private TextView startTimeText;
+    private TextView endTimeText;
+
+    int startYear;
+    int endYear;
+
+    int startMonth;
+    int endMonth;
+
+    int startDay;
+    int endDay;
+
+    int startHour;
+    int endHour;
+
+    int startMinute;
+    int endMinute;
+
+    private Calendar calendar;
     public TimeChooserFragment() {
         // Required empty public constructor
     }
@@ -52,8 +91,7 @@ public class TimeChooserFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -61,6 +99,118 @@ public class TimeChooserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_chooser, container, false);
+        View view = inflater.inflate(R.layout.fragment_time_chooser, container, false);
+        monthOuterLayout = view.findViewById(R.id.monthOuterLayout);
+        monthInnerLayout = view.findViewById(R.id.monthInnerLayout);
+        dayOuterLayout = view.findViewById(R.id.dayOuterLayout);
+        dayInnerLayout = view.findViewById(R.id.dayInnerLayout);
+        timeOuterLayout = view.findViewById(R.id.timeOuterLayout);
+        timeInnerLayout = view.findViewById(R.id.timeInnerLayout);
+
+        monthSwitch = view.findViewById(R.id.monthSwitch);
+        daySwitch = view.findViewById(R.id.daySwitch);
+        timeSwitch = view.findViewById(R.id.timeSwitch);
+
+        monthSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                monthSwitchController(b);
+            }
+        });
+
+        daySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                daySwitchController(b);
+            }
+        });
+
+        timeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                timeSwitchController(b);
+            }
+        });
+
+        startTimePicker = view.findViewById(R.id.startTimePicker);
+        endTimePicker = view.findViewById(R.id.endTimePicker);
+        startTimeText = view.findViewById(R.id.startTimeText);
+        endTimeText = view.findViewById(R.id.endTimeText);
+        calendar = Calendar.getInstance();
+
+        startTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog tp1 = new TimePickerDialog(getContext(),new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        startHour = i;
+                        startMinute = i1;
+                        startTimeText.setText(HourMinuteHandler.combine(i,i1));
+                        Log.d("TAGG",HourMinuteHandler.combine(i,i1));
+                    }
+
+                }, startHour, startMinute, true);
+                tp1.setButton(TimePickerDialog.BUTTON_POSITIVE, "Pick", tp1);
+                tp1.setButton(TimePickerDialog.BUTTON_NEGATIVE, "Cancel", tp1);
+                tp1.show();
+            }
+        });
+
+        endTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog tp1 = new TimePickerDialog(getContext(),new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        endHour = i;
+                        endMinute = i1;
+                        endTimeText.setText(HourMinuteHandler.combine(i,i1));
+                    }
+                }, endHour, endMinute, true);
+                tp1.setButton(TimePickerDialog.BUTTON_POSITIVE, "Pick", tp1);
+                tp1.setButton(TimePickerDialog.BUTTON_NEGATIVE, "Cancel", tp1);
+                tp1.show();
+            }
+        });
+
+        return view;
+    }
+
+    private void monthSwitchController(boolean isChecked){
+        if (isChecked){
+            monthInnerLayout.setVisibility(View.VISIBLE);
+            dayOuterLayout.setVisibility(View.VISIBLE);
+            dayInnerLayout.setVisibility(View.INVISIBLE);
+        }
+        else{
+            monthInnerLayout.setVisibility(View.INVISIBLE);
+            dayOuterLayout.setVisibility(View.INVISIBLE);
+            daySwitch.setChecked(false);
+        }
+
+
+    }
+
+    private void daySwitchController(boolean isChecked){
+        if (isChecked){
+            dayInnerLayout.setVisibility(View.VISIBLE);
+            timeOuterLayout.setVisibility(View.VISIBLE);
+        }
+        else{
+            dayInnerLayout.setVisibility(View.INVISIBLE);
+            timeOuterLayout.setVisibility(View.INVISIBLE);
+            timeSwitch.setChecked(false);
+        }
+    }
+
+    private void timeSwitchController(boolean isChecked){
+        if (isChecked){
+            timeInnerLayout.setVisibility(View.VISIBLE);
+            timeOuterLayout.setVisibility(View.VISIBLE);
+        }
+        else{
+            timeInnerLayout.setVisibility(View.INVISIBLE);
+        }
     }
 }
