3f9b3c08192d2d669c654b9b1878eabb561c033b
Niyazi Ülke <niyaziulke@gmail.com>
2021-12-11 22:08:21 +0300

    Other profile page activity created

---
+++ postory/android/Postory/app/src/main/java/com/example/postory/activities/OtherProfilePageActivity.java
@@ -0,0 +1,16 @@
+package com.example.postory.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.postory.R;
+
+public class OtherProfilePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_profile_page);
+    }
+}
