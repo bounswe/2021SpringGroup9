9a6f3b2be7ed2e3b2aa7c182f3d0bb50ae0299d4
Ahmet Melih Aydoğdu <ahaydogdu@hotmail.com>
2021-12-11 20:55:21 +0300

    cors origins fixed

--- postory/backend/backend/settings.py
+++ postory/backend/backend/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['http://3.125.114.231:3000', 'http://3.125.114.231:8000']
 
 # Application definition
 
