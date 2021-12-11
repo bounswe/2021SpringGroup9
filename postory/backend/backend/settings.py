3f20d22272fcc128c482b78f710b143ae5d4c020
Ahmet Melih Aydoğdu <ahaydogdu@hotmail.com>
2021-12-11 21:39:06 +0300

    origins updated

--- postory/backend/backend/settings.py
+++ postory/backend/backend/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = ['http://3.125.114.231:3000', 'http://3.125.114.231:8000']
+CORS_ALLOWED_ORIGINS = ['http://3.125.114.231:3000', 'http://3.125.114.231:8000', 'http://localhost:3000', 'http://localhost:8000']
 
 CORS_ALLOW_CREDENTIALS = True
 
