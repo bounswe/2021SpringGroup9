f7bf96d8be011250f9c3afbb07620bab411b21ba
Ahmet Şentürk <iahmetsenturk@yahoo.com>
2021-12-12 20:21:08 +0300

    router path added for the profile page

--- postory/frontend/src/index.js
+++ postory/frontend/src/index.js
@@ -19,6 +19,7 @@ import {
 import ForgotPassword from "./ForgotPassword";
 import ForgotPasswordConfirm from "./ForgotPasswordConfirm";
 import Activation from "./Activation";
+import { ProfilePageUpper } from './ProfilePage';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -33,6 +34,7 @@ ReactDOM.render(
     <Route path="/password/reset/confirm/:uid/:token" element={<div><TopBar /><ForgotPasswordConfirm /></div>} />
     <Route path="/activate/:uid/:token" element={<div><TopBar /><Activation /></div>} />
     <Route path="/viewPost" element={<div><TopBar/> <ViewPost /></div>} />
+    <Route path="/profilePage" element={<div><TopBar/> <ProfilePageUpper /></div>} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
