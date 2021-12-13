08a0e806f5695a2bd6272369954d676f25f2c48f
mecolak <mstfec@gmail.com>
2021-12-14 01:39:30 +0300

    Post preview does not crash even if all the fields are empty. User is redirected to the sign in page if not logged in or token is invalid.

--- postory/frontend/src/Redirector.js
+++ postory/frontend/src/Redirector.js
@@ -1,13 +1,21 @@
 
 import React from "react";
 import { Link, Navigate} from "react-router-dom";
+import * as requests from './requests'
 class Redirector extends React.Component{
 
     constructor(props){
         super(props);
+        let error = false;
         this.state = {
-            hasJWT: localStorage.getItem("access")
+            hasJWT: !error && localStorage.getItem("access")
         }
+        // Redirect if not able to get all posts.
+        requests.get_jwt('/api/post/all',{}).
+        then(resp => 
+            this.setState( {hasJWT: (resp.status==200) && localStorage.getItem("access")})
+            ).catch(err => console.log(err));
+        
     }
 
     render(){
