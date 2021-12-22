81d6b8552c7db47427be6e9ab035092f6189f5c2
zcanfes <zcanfes@gmail.com>
2021-12-22 10:42:15 +0300

    Authentication of another user follow test fixed

--- postory/backend/user_endpoint/tests.py
+++ postory/backend/user_endpoint/tests.py
@@ -3,7 +3,6 @@ from user_endpoint.models import User
 from rest_framework.test import APIClient, APITestCase
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.urls import reverse
-import pytest
 import json 
 
 
@@ -58,19 +57,12 @@ class TestFollow(APITestCase):
    
 
     def test_another_user_follow(self):
-        # user_data = {
-        #     "email":'user4@email.com', "password":'Password123%', "re_password":"Password123%","username":"user4", "name":'Name4', "surname":'Surname4'
-        # }
-
-        # res = self.client.post('/auth/users/', json.dumps(user_data), content_type='application/json')
-        # print(res)        
-
         login_data = {
             "email": "user3@email.com",
             "password": "Password123%"
         }
-        token = self.client.post('/auth/jwt/create/', json.dumps(login_data), content_type='application/json')
-        self.client.credentials(Authorization=token.data['access'])
+        token = self.client.post("/auth/jwt/create",login_data)
+        print(token)
 
         response = self.client.post(reverse('follow_user', args=[self.user1.id]), format='json')
         assert response.status_code == 200
