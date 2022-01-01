98ed63899bd1b89ead93873e9adf23b655d7e15e
Melih Özcan <melihozcan98@hotmail.com>
2022-01-01 18:31:19 +0300

    Preview posts on discovery page implemented.

--- postory/android/Postory/app/src/main/java/com/example/postory/activities/ExploreActivity.java
+++ postory/android/Postory/app/src/main/java/com/example/postory/activities/ExploreActivity.java
@@ -6,8 +6,16 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.postory.BuildConfig;
 import com.example.postory.R;
 import com.example.postory.adapters.PostAdapter;
@@ -37,14 +45,17 @@ public class ExploreActivity extends  ToolbarActivity implements OnMapReadyCallb
 
     private Post[] posts;
     private Request request;
+    private Request requestGetPost;
     private OkHttpClient client;
     private String url;
+    private String urlGetPost;
     private static final String  TAG = "ExploreActivity";
     private GoogleMap mMap;
     private SharedPreferences sharedPreferences;
     private String accessToken;
     private  ArrayList<Post> arrayOfPosts;
     private HashMap<String, Integer > markerId = new HashMap<>();
+    private Post post;
 
     @Override
     protected void goHomeClicked() {
@@ -110,6 +121,8 @@ public class ExploreActivity extends  ToolbarActivity implements OnMapReadyCallb
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        
+       
 
     }
 
@@ -148,8 +161,6 @@ public class ExploreActivity extends  ToolbarActivity implements OnMapReadyCallb
 
                                         markerId.put(m.getId(),post.getId());
 
-
-
                                     }
                                 });
 
@@ -179,6 +190,61 @@ public class ExploreActivity extends  ToolbarActivity implements OnMapReadyCallb
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(@NonNull Marker marker) {
+                Log.i(TAG, "onInfoWindowClick: ");
+                Intent i = new Intent(ExploreActivity.this, SinglePostActivity.class);
+                i.putExtra("post_id",markerId.get(marker.getId()) + "");
+                startActivity(i);
+            }
+        });
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Nullable
+            @Override
+            public View getInfoContents(@NonNull Marker marker) {
+                View v = getLayoutInflater().inflate(R.layout.single_post_preview, null);
+                v.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i(TAG, "onClick: ");
+                                
+                    }
+                });
+
+                ImageView imagePost = (ImageView) v.findViewById(R.id.post_photo_preview);
+                TextView textTitle = (TextView) v.findViewById(R.id.title_preview);
+                TextView textContent = (TextView) v.findViewById(R.id.content_preview);
+
+                Post holdThePost = new Post();
+                for(Post post : arrayOfPosts) {
+                    if(post.getId() == markerId.get(marker.getId())) {
+                        holdThePost = post;
+                        break;
+                    }
+                }
+
+                textTitle.setText(holdThePost.getTitle());
+                textContent.setText(holdThePost.getStory());
+                if(holdThePost.getImages().size() != 0) {
+                    Glide
+                            .with(ExploreActivity.this)
+                            .load(holdThePost.getImages().get(0))
+                            .placeholder(R.drawable.placeholder)
+                            .apply(new RequestOptions().override(400,400))
+                            .centerCrop()
+                            .into(imagePost);
+                }
+
+                return v;
+            }
+
+            @Nullable
+            @Override
+            public View getInfoWindow(@NonNull Marker marker) {
+                return null;
+            }
+        });
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(41.085097, 29.043101), 10));
         mMap.setOnMarkerClickListener(ExploreActivity.this);
         mMap.setOnMapLongClickListener(ExploreActivity.this);
@@ -186,22 +252,12 @@ public class ExploreActivity extends  ToolbarActivity implements OnMapReadyCallb
 
     @Override
     public boolean onMarkerClick(@NonNull @NotNull Marker marker) {
-
-
-        Intent i = new Intent(ExploreActivity.this, SinglePostActivity.class);
-        i.putExtra("post_id",markerId.get(marker.getId()) + "");
-        startActivity(i);
-
-
+        marker.showInfoWindow();
         return  true;
     }
 
     @Override
     public void onMapLongClick(@NonNull @NotNull LatLng latLng) {
-
-
-
-
         mMap.addMarker(new MarkerOptions()
                 .position(latLng)
                 .title("New Marker"));
