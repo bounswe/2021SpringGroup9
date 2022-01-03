452b078bc5e2ffb2e45db529c8a0198eab2f1d07
Melih Özcan <melihozcan98@hotmail.com>
2022-01-03 20:59:07 +0300

     Create the layout for the activity stream

---
+++ postory/android/Postory/app/src/main/java/com/example/postory/activities/ActivityStreamActivity.java
@@ -0,0 +1,45 @@
+package com.example.postory.activities;
+
+
+import android.os.Bundle;
+
+import com.example.postory.R;
+
+public class ActivityStreamActivity extends ToolbarActivity  {
+    @Override
+    protected void goHomeClicked() {
+
+    }
+
+    @Override
+    protected void goCreatePostClicked() {
+
+    }
+
+    @Override
+    protected void refreshClicked() {
+
+    }
+
+    @Override
+    protected void goExploreClicked() {
+
+    }
+
+    @Override
+    protected void goProfileClicked() {
+
+    }
+
+    @Override
+    protected void logoutClicked() {
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
+}
