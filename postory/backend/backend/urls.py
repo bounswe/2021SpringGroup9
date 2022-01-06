e791df00a2ca16e0b3e84c6047ea09faafef9867
zcanfes <zcanfes@gmail.com>
2022-01-06 23:10:57 +0300

    Swagger settings added

--- postory/backend/backend/urls.py
+++ postory/backend/backend/urls.py
@@ -18,6 +18,19 @@ from django.contrib import admin
 from django.urls import path, re_path
 from post_endpoint.urls import *
 from django.views.generic import TemplateView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Postory API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +38,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('api/user/', include('user_endpoint.urls')),
-    path('api/activitystream/', include('activityStream.urls'))
+    path('api/activitystream/', include('activityStream.urls')),
+    path('api/activitystream/', include('activityStream.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
